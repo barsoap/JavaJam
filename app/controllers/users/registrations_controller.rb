919cb4491bb050b6b794c1b5b1e6a876b6804194
EC2 Default User <ec2-user@ip-172-31-0-218.ap-northeast-1.compute.internal>
2024-04-08 16:35:28 +0000

    [Add]bootstrap

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   def after_sign_up_path_for(resource)
@@ -43,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -67,6 +67,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
    #ストロングパラメーターを設定(customerカラムの内容を許可する
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :is_active])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
