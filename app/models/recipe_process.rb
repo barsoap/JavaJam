22db919390a5e80da78696b195541a11899c0569
barsoap <ftcb1999@gmail.com>
2024-04-15 07:06:43 +0000

    [Add]migtrate

---
+++ app/models/recipe_process.rb
@@ -0,0 +1,2 @@
+class RecipeProcess < ApplicationRecord
+end
---
+++ db/migrate/20240415065002_create_recipe_processes.rb
@@ -0,0 +1,12 @@
+class CreateRecipeProcesses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipe_processes do |t|
+      
+      t.integer :recipe_id, null: false
+      t.string :process, null: false
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
