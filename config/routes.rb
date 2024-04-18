0e48aa773d6f68cf335b9d0219e231956cfba942
barsoap <ftcb1999@gmail.com>
2024-04-18 10:13:06 +0000

    [Add]guest-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   # devise_for :admins
   # devise_for :users
   # 顧客用
