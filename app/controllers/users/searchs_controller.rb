50c5a3736f1a48dfaab282e694005488907244f7
barsoap <ftcb1999@gmail.com>
2024-05-23 11:57:11 +0000

    [Add]bookmark sort

--- app/controllers/users/searchs_controller.rb
+++ app/controllers/users/searchs_controller.rb
@@ -27,11 +27,23 @@ class Users::SearchsController < ApplicationController
       @recipes = Recipe.search_for(@content).order(created_at: :asc)
     when 'evaluation'
       @recipes = Recipe.search_for(@content).order(evaluation: :desc)
+    when 'redipe_bookmark'
+      @recipes = Recipe.search_for(@content).sort {|a,b| b.recipe_bookmarks.size <=> a.recipe_bookmarks.size}
     else
       @recipes = Recipe.search_for(@content).order(created_at: :desc)
     end
 
-    @equipments = Equipment.search_for(@content)
+    case params[:equipment_sort]
+    when 'equipment_latest'
+      @equipments = Equipment.search_for(@content).order(created_at: :desc)
+    when 'equipment_oldest'
+      @equipments = Equipment.search_for(@content).order(created_at: :asc)
+    when 'evaluation'
+      @equipments = Equipment.search_for(@content).order(evaluation: :desc)
+    else
+      @equipments = Equipment.search_for(@content).order(created_at: :desc)
+    end
+
     @notes = Note.search_for(@content)
 
   end
