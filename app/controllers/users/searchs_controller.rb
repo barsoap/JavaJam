0a1940ea54b0cd03e1cf957ec968c4abfb880862
barsoap <ftcb1999@gmail.com>
2024-05-28 14:50:23 +0000

    [Fix]comment out

--- app/controllers/users/searchs_controller.rb
+++ app/controllers/users/searchs_controller.rb
@@ -14,7 +14,7 @@ class Users::SearchsController < ApplicationController
       @recipes = Recipe.search_for(@content).order(created_at: :asc)
     when 'evaluation'
       @recipes = Recipe.search_for(@content).order(evaluation: :desc)
-    when 'redipe_bookmark'
+    when 'recipe_bookmark'
       @recipes = Recipe.search_for(@content).sort {|a,b| b.recipe_bookmarks.size <=> a.recipe_bookmarks.size}
     else
       @recipes = Recipe.search_for(@content).order(created_at: :desc)
--- app/models/note.rb
+++ app/models/note.rb
@@ -8,7 +8,8 @@ class Note < ApplicationRecord
   def self.search_for(content)
     # Note
     # .joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = notes.id AND action_text_rich_texts.record_type = 'Note'").to_sql
-
+    
+    #SQL文で内部結合して、直接NoteモデルのデータとActionTextのデータのidが同じものを取得
     notes = ActiveRecord::Base.connection.select_all('SELECT * FROM "notes" INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = notes.id AND action_text_rich_texts.record_type = "Note"')
 
     # note_ids = []
@@ -17,12 +18,12 @@ class Note < ApplicationRecord
     #     note_ids << note['id']
     #   end
     # end
-
+    
+    #その中からタイトルか本文(ActionText)の内容(ここでHTMLタグを除く)で合致するものを取得
     note_ids = notes.map {|note| note['id'] if note['title'].match(content)}
     note_ids += notes.map {|note| note['id'] if ApplicationController.helpers.strip_tags(note['body']).gsub(/[\r\n]/,"").match(content)}
 
     Note.where(id: note_ids)
-
 
     # .where("title LIKE ? OR action_text_rich_texts.body LIKE ?", "%#{content}%", "%#{content}%")
   end
